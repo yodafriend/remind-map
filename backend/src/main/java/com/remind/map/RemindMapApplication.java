d5a27344eba533e9105b2064a56832141aa69b09
dbstn1325 <dbstn1325@naver.com>
2023-10-27 17:52:49 +0900

    feat: 백엔드 프로젝트 생성

---
+++ backend/src/main/java/com/remind/map/RemindMapApplication.java
@@ -0,0 +1,13 @@
+package com.remind.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemindMapApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/remind/map/RemindMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remind.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
