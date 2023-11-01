0bf59bcd86c2fe11af613fb910dce74592ed2fa8
summermong <2514o_o@naver.com>
2023-11-02 00:35:22 +0900

    DESIGN: 헤더/사이드바 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Header from './common/Header';
+import Sidebar from './common/Sidebar';
 
 function App() {
-  return <div className="App">Hello remindmap</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
