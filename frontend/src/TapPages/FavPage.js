ff4fa554a58b39b868ec3b7a2665eb59eb468a32
summermong <2514o_o@naver.com>
2023-11-02 00:31:49 +0900

    DESIGN: 찜 탭 작업중
    앞으로 작업을 해야합니다.

---
+++ frontend/src/TapPages/FavPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './FavPage.module.css';
+
+const FavPage = () => {
+  return <div className={styles.favPage}>FavPage</div>;
+};
+
+export default FavPage;
