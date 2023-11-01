0f7c7f673f48ae70e188bdcfdcf5688b622da358
summermong <2514o_o@naver.com>
2023-11-02 00:32:53 +0900

    DESIGN: 그룹 탭 목업
    경규님 작업 파트입니다.

---
+++ frontend/src/TapPages/GroupPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './GroupPage.module.css';
+
+const GroupPage = () => {
+  return <div className={styles.groupPage}>GroupPage</div>;
+};
+
+export default GroupPage;
