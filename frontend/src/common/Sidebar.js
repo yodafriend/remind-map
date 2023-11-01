fec7b5b49fa7bd18a91b4ceca17304f1da6d2b65
summermong <2514o_o@naver.com>
2023-11-02 00:26:49 +0900

    DESIGN: 사이드바 작업 완료
    - 헤더와 그림자 효과가 겹쳐서 빼는 게 좋을지 고민입니다. :(
    - 토글처럼 되어 있는데 애니메이션 기능 추가할 예정입니다.

---
+++ frontend/src/common/Sidebar.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Styles from './Sidebar.module.css';
+import SidebarMenu from './SidebarMenu';
+import SidebarContent from './SidebarContent';
+
+const OpenBtn = ({ handleSidebarToggle, isSidebarOpen }) => {
+  return (
+    <div>
+      <button className={Styles.openBtn} onClick={handleSidebarToggle}>
+        {isSidebarOpen ? '◀' : '▶'}
+      </button>
+    </div>
+  );
+};
+
+const Sidebar = () => {
+  const [selectedTab, setSelectedTab] = useState('검색');
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleSidebarToggle = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  // eslint-disable-next-line prettier/prettier
+  const handleTabChange = tab => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div className={Styles.sidebarContainer}>
+      <div className={Styles.sidebar}>
+        {isSidebarOpen && (
+          <>
+            <SidebarMenu handleTabChange={handleTabChange} />
+            <SidebarContent selectedTab={selectedTab} />
+          </>
+        )}
+      </div>
+      <OpenBtn
+        handleSidebarToggle={handleSidebarToggle}
+        isSidebarOpen={isSidebarOpen}
+      />
+    </div>
+  );
+};
+
+export default Sidebar;
