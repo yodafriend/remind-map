9f28d7256ef68753fa304783aab72b90bfbfab38
summermong <2514o_o@naver.com>
2023-11-02 00:34:04 +0900

    DESIGN: 마커/그룹 탭 목업
    요나님 파트입니다.

---
+++ frontend/src/TapPages/MarkerPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './MarkerPage.module.css';
+
+const MarkerPage = () => {
+  return <div className={styles.markerPage}>MarkerPage</div>;
+};
+
+export default MarkerPage;
---
+++ frontend/src/TapPages/RoutePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './RoutePage.module.css';
+
+const RoutePage = () => {
+  return <div className={styles.routePage}>RoutePage</div>;
+};
+
+export default RoutePage;
