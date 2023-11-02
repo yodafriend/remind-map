e1f54c7cfb723ae25fe8de6183b97f063c5e5b26
강경규 <qwzx16@naver.com>
2023-11-02 18:40:16 +0900

    TEST: 그룹 페이지 링크 임시 추가 - 작업 편하게 하기 위함 추 후 삭제 예정

--- frontend/src/common/Header.js
+++ frontend/src/common/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Styles from './Header.module.css';
 import { AiFillHome } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className={Styles.header}>
       <div className={Styles.title}>📌 RemindMap</div>
+      <Link to="/group">그룹</Link>
       <div className={Styles.userInfo}>
         <div className={Styles.userPhoto} />
         <div className={Styles.userName}>홍사미</div>
