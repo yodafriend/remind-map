1dc7657b8617ffd53cac4f4d8d3cf53e9661208b
강경규 <qwzx16@naver.com>
2023-11-02 16:35:40 +0900

    FEAT: 그룹페이지 라우팅을 위한 Router 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Header from './common/Header';
 import Sidebar from './common/Sidebar';
-
+import { Routes, Route } from 'react-router-dom';
+import Group from './pages/Group/GroupPage';
 function App() {
   return (
     <div className="App">
       <Header />
       <Sidebar />
-      //TESTasd
+      <Routes>
+        <Route path="/group" element={<Group />} />
+      </Routes>
     </div>
   );
 }
