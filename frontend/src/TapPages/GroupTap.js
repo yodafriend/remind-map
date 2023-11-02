f8726e8bbe22db67d328eefd9f266d9114e75de4
강경규 <qwzx16@naver.com>
2023-11-02 16:36:36 +0900

    STYLE: 버튼 컴포넌트 추가

--- frontend/src/TapPages/GroupTap.js
+++ frontend/src/TapPages/GroupTap.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './GroupTap.module.css';
-
+import MakeBtn from '../common/MakeBtn';
 const GroupTap = () => {
-  return <div className={styles.groupTap}>GroupTap</div>;
+  return (
+    <div className={styles.groupTap}>
+      <MakeBtn text="그룹 관리" />
+    </div>
+  );
 };
 
 export default GroupTap;
