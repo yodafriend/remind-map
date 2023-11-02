15df38369aa93054823f60d0b1dfde9d6023fe48
강경규 <qwzx16@naver.com>
2023-11-02 18:39:32 +0900

    FEAT: 라우팅 수정 - 그룹 페이지에선 사이드바가 불필요하기 때문에 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import Header from './common/Header';
 import Sidebar from './common/Sidebar';
 import { Routes, Route } from 'react-router-dom';
-import GroupPage from './Group/pages/GroupPage';
+import GroupHome from './group/pages/GroupHome';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Sidebar />
       <Routes>
-        <Route path="/group" element={<GroupPage />} />
+        <Route path="/" element={<Sidebar />} />
+        <Route path="/group" element={<GroupHome />} />
       </Routes>
     </div>
   );
