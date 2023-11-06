c1a4b646bd5105001ef5ecdafaa88e704cb48198
강경규 <qwzx16@naver.com>
2023-11-06 22:42:47 +0900

    FEAT: Datepicker 추가
    tailwindCSS 이용하여 설정 추가

--- frontend/src/TapPages/GroupTap.js
+++ frontend/src/TapPages/GroupTap.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './GroupTap.module.css';
 import MakeBtn from '../common/MakeBtn';
+import Datepicker from 'react-tailwindcss-datepicker';
 import { Link, useNavigate } from 'react-router-dom';
 
 const GroupTap = () => {
   const navigete = useNavigate();
-  const isGroup = () => {
-    navigete('/group');
+  const [value, setValue] = useState({
+    startDate: new Date(),
+    endDate: new Date().setMonth(11),
+  });
+
+  const handleValueChange = newValue => {
+    console.log('newValue:', newValue);
+    setValue(newValue);
   };
+
   return (
     <div className={styles.groupTap}>
       <MakeBtn text="그룹 관리" />
+      <Datepicker
+        inputClassName="w-full p-2"
+        containerClassName={`${styles.test} border rounded-sm`}
+        toggleClassName="hidden"
+        primaryColor="purple"
+        useRange={false}
+        value={value}
+        onChange={handleValueChange}
+        placeholder="YYYY-MM-DD"
+      />
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: 'class',
 };
