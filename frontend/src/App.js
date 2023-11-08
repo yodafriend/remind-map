e086bcecdd6b1db1c953d84f04c455ff5cad825b
summermong <2514o_o@naver.com>
2023-11-08 15:24:35 +0900

    FEAT: PostingModal 확인용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Main from './common/Main';
 import Header from './common/Header';
 import Sidebar from './common/Sidebar';
 import Redirect from './api/Redirect';
+import PostingModal from './userPosting/PostingModal';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route exact path="/kakao/callback" element={<Redirect />} />
         </Routes>
+        <PostingModal /> {/* 확인용 */}
         <Sidebar />
       </div>
     </BrowserRouter>
