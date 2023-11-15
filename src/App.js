d0ad00db1822cc1af8e00650c7cafbe5b6e98a01
summermong <2514o_o@naver.com>
2023-11-15 15:49:06 +0900

    FEAT: Recoil 라이브러리 설치

--- src/App.js
+++ src/App.js
@@ -6,26 +6,29 @@ import Sidebar from './common/frame/Sidebar';
 import Redirect from './api/Redirect';
 import PostingModal from './common/userposting/PostingModal';
 import GroupHome from './tap/group/pages/GroupHome';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Main /> <Sidebar />
-              </>
-            }
-          />
-          <Route path="/group" element={<GroupHome />} />
-          <Route exact path="/kakao/callback" element={<Redirect />} />
-        </Routes>
-        <PostingModal /> {/* 확인용 */}
-      </div>
+      <RecoilRoot>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Main /> <Sidebar />
+                </>
+              }
+            />
+            <Route path="/group" element={<GroupHome />} />
+            <Route exact path="/kakao/callback" element={<Redirect />} />
+          </Routes>
+          <PostingModal /> {/* 확인용 */}
+        </div>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
