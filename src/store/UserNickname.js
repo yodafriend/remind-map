f6f680ee0533030d8ba0bfe914466b10f0a2cf7d
summermong <2514o_o@naver.com>
2023-11-15 15:48:28 +0900

    FEAT: 로그인 시 전역 상태 데이터 추가

---
+++ src/store/UserNickname.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const UserNickname = atom({
+  key: 'UserNickname',
+  default: '',
+});
---
+++ src/store/UserProfile.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const UserProfile = atom({
+  key: 'UserProfile',
+  default: '',
+});
