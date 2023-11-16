40c9e324264fd2bf4d2d3dda61f099eefda1c190
summermong <2514o_o@naver.com>
2023-11-16 19:23:21 +0900

    FEAT: 검색 기능 추가로 props 변경

--- src/common/input/SearchInput.js
+++ src/common/input/SearchInput.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Styles from './SearchInput.module.css';
 
-const SearchInput = () => {
+const SearchInput = ({ value, onChange }) => {
   return (
     <div className={Styles.SearchInputContainer}>
-      <input className={Styles.searchInput} placeholder="장소를 입력하세요." />
+      <input
+        className={Styles.searchInput}
+        placeholder="장소를 입력하세요."
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
--- src/common/userposting/Posting.js
+++ src/common/userposting/Posting.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Styles from './Posting.module.css';
 import { AiFillStar } from 'react-icons/ai';
 
-const Posting = ({ title, writer, date, fav }) => {
+const Posting = ({ title, writer, wentDate, fav, latitude, longitude }) => {
   const [onFav, setOnFav] = useState(fav);
 
   const handleFav = () => {
@@ -14,7 +14,9 @@ const Posting = ({ title, writer, date, fav }) => {
       <div className={Styles.postingInfo}>
         <div className={Styles.title}>{title}</div>
         <div className={Styles.writer}>{writer}</div>
-        <div className={Styles.date}>{date}</div>
+        <div className={Styles.wentDate}>{wentDate}</div>
+        <div className={Styles.latitude}>{latitude}</div>
+        <div className={Styles.longitude}>{longitude}</div>
       </div>
       <button className={Styles.fav} onClick={handleFav}>
         <AiFillStar className={`${Styles.fav} ${onFav ? Styles.active : ''}`} />
