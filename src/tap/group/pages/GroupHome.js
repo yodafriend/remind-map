cef7616b0bb2c0a4d79beb377e68b833294e461d
강경규 <qwzx16@naver.com>
2023-11-20 16:06:09 +0900

    FEAT: 그룹페이지 url별로 라우팅

--- src/tap/group/pages/GroupHome.js
+++ src/tap/group/pages/GroupHome.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import GroupList from '../components/GroupList';
-import GroupDetail from '../components/GroupDetail';
-import GroupCreate from '../components/GroupCreate';
+import { useMatch } from 'react-router-dom';
+
+import GroupList from '../components/ui-components/GroupList';
+import GroupDetail from '../components/ui-components/GroupDetail';
+import GroupCreate from '../components/ui-components/GroupCreate';
+import GroupFriends from '../components/ui-components/GroupFriends';
 
 export default function GroupHome() {
+  const create = useMatch('/group/create');
+  const detail = useMatch('/group/detail/:id');
+
   return (
     <div>
-      <div className="flex mt-10">
+      <div className="flex pt-10">
         <GroupList />
-        <div className="w-full flex items-centers justify-center">
-          <GroupDetail />
-          <GroupCreate />
+        <div className="w-full flex flex-col md:flex-row items-centers justify-around">
+          {create && <GroupCreate />}
+          {detail && (
+            <>
+              <GroupDetail /> <GroupFriends />
+            </>
+          )}
         </div>
       </div>
     </div>
