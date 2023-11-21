20dac3f9f7acfe8d9fd01e8224af2605eaec080d
강경규 <qwzx16@naver.com>
2023-11-21 17:24:18 +0900

    FEAT: 기능추가X

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { RecoilRoot } from 'recoil';
 
 import { worker } from './mooks/worker';
-import { RecoilRoot } from 'recoil';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start({
