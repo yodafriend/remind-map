53e029ca40a079b28b8ab510d50c355268b186c8
summermong <2514o_o@naver.com>
2023-11-24 02:46:25 +0900

    CHORE: 불필요한 import 삭제

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Main from './common/frame/Main';
 import Header from './common/frame/Header';
 import Sidebar from './common/frame/Sidebar';
 import Redirect from './api/Redirect';
-import PostingModal from './common/userposting/PostingModal';
-import GroupHome from './tap/group/pages/GroupHome';
 import { RecoilRoot } from 'recoil';
 
 function App() {
