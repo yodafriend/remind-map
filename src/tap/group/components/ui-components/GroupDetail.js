63150f1157749fe277e5c6b7834b80d5375bcca8
강경규 <qwzx16@naver.com>
2023-11-25 23:49:35 +0900

    FEAT: Input, Button 옵션 추가 , 초대관련 로직 추가

--- src/tap/group/components/ui-components/GroupDetail.js
+++ src/tap/group/components/ui-components/GroupDetail.js
@@ -12,18 +12,27 @@ export default function GroupDetail() {
   const { groupId } = useParams();
   const group = useRecoilValue(groupState);
   const [search, setSearch] = useState('');
-  const { getGroup, getGroupmembers, groupMembers } = useGroup(groupId);
+  const [groupTitle, setGroupTitle] = useState('');
+  const { getGroup, getGroupmembers, groupMembers, deletGroup, editGroup } = useGroup(
+    groupId,
+    groupTitle,
+  );
 
   useEffect(() => {
     getGroupmembers();
     getGroup();
-    console.log(groupMembers);
   }, [groupId]);
 
   return (
-    <div className="flex flex-col items-center gap-5 mt-20 md:mt-10 transition-all">
+    <div className="flex flex-col items-center gap-5 mt-3 transition-all">
       <h1 className=" text-4xl">{group.groupTitle}</h1>
-      <GroupInput setValue={setSearch} buttonText="검색" />
+      <GroupInput
+        setValue={setGroupTitle}
+        buttonText="수정"
+        placeholder="그룹 이름"
+        buttonOnclick={editGroup}
+      />
+      <GroupInput setValue={setSearch} buttonText="검색" placeholder="그룹원 찾기" />
       {groupMembers.length !== 0 ? (
         <GroupUserCardWraper
           mambers={groupMembers}
@@ -41,7 +50,7 @@ export default function GroupDetail() {
       )}
 
       <div className="flex items-center justify-center gap-2 w-full">
-        <GroupButton type="Button" text="그룹 삭제" />
+        <GroupButton type="Button" text="그룹 삭제" onClick={deletGroup} />
       </div>
     </div>
   );
--- src/tap/group/components/ui-components/GroupFriends.js
+++ src/tap/group/components/ui-components/GroupFriends.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import GroupButton from '../atom-components/GroupButton';
 import GroupInput from '../atom-components/GroupInput';
@@ -8,32 +8,35 @@ import { useParams } from 'react-router-dom';
 
 export default function GroupFriends() {
   const { groupId } = useParams();
-  const { getFriends, friends } = useFriends();
+  const [member, setMember] = useState({});
+  const { getFriends, friends, sendMessage, sendInGroup } = useFriends(member);
+
   useEffect(() => {
     getFriends();
   }, [groupId]);
-  const sendMessage = member => {
-    const uuids = [];
-    uuids.push(member.uuid);
-    console.log(uuids);
+  const submitInvite = member => {
+    setMember(member);
+    sendMessage(member);
+    sendInGroup(member);
   };
+
   return (
     <div className="flex flex-col items-center gap-5 mt-20 md:mt-10 transition-all">
       <h1 className="text-xl">초대할수 있는 친구목록</h1>
 
-      <GroupInput buttonText="검색" placeholder="친구 이름" />
+      <GroupInput buttonText="검색" placeholder="친구 찾기" />
       <GroupUserCardWraper
         mambers={friends}
         title="친구 목록"
         buttonText="초대"
-        buttonOnClick={sendMessage}
+        buttonOnClick={submitInvite}
       />
-
-      <GroupButton text="카카오 친구 초대" type="Button" />
       <div className="border w-full p-3 text-center rounded-md">
         <p>카카오톡 친구를</p>
         <p>초대해보세요!</p>
       </div>
+
+      <GroupButton text="카카오 친구 초대" type="Button" />
     </div>
   );
 }
