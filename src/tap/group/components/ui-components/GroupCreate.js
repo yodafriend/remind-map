b5ca48891cb367261f79d5d24febb44ef5903303
강경규 <qwzx16@naver.com>
2023-11-26 02:55:34 +0900

    REFACTOR: 그룹생성 로직 분리

--- src/tap/group/components/ui-components/GroupCreate.js
+++ src/tap/group/components/ui-components/GroupCreate.js
@@ -1,49 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import GroupInput from '../atom-components/GroupInput';
 import useGroup from '../../../../hooks/useGroup';
 import { useMatch, useNavigate } from 'react-router-dom';
-import { seletGroupIdState } from '../../../../recoil/groupAtoms';
-import { useSetRecoilState } from 'recoil';
-import { instance } from '../../../../api/customAxios';
 
 export default function GroupCreate() {
   const [groupTitle, setGroupTitle] = useState('');
-  const { getGroups } = useGroup();
-  const setSeletGroupId = useSetRecoilState(seletGroupIdState);
+  const { createGroup } = useGroup();
+
   const navigate = useNavigate();
   const isTabCreate = useMatch('/grouptab/create/:id');
   const isCreate = useMatch('/group/create/:id');
-  useEffect(() => {
-    console.log(groupTitle);
-  }, [groupTitle]);
-  const createGroup = async () => {
-    console.log('그룹제목 : ', groupTitle);
-    try {
-      const result = await instance.post('/group/create', { title: groupTitle });
-      console.log(result);
-      // getGroups();
-      // if (isTabCreate) {
-      //   navigate(`/grouptab/all/${result.data.groupId}`);
-      //   setSeletGroupId(result.data.groupId);
-      // }
-      // if (isCreate) {
-      //   navigate(`/group/detail/${result.data.groupId}`);
-      //   setSeletGroupId(result.data.groupId);
-      // }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div className="flex flex-col items-center gap-5 mt-20 md:mt-10 transition-all">
       <h1 className="text-4xl">그룹 생성</h1>
       <form className="flex flex-col items-center justify-center gap-5 w-full">
         <GroupInput
           setValue={setGroupTitle}
-          keyDown={createGroup}
+          keyDown={() => createGroup(isTabCreate, isCreate, navigate, groupTitle)}
           buttonText="생성"
-          buttonOnclick={createGroup}
+          buttonOnclick={() => createGroup(isTabCreate, isCreate, navigate, groupTitle)}
           placeholder="그룹 이름"
         />
       </form>
