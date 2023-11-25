aeba16cb6e41237940483d8348b73fdb2f546150
강경규 <qwzx16@naver.com>
2023-11-25 23:43:34 +0900

    FEAT: 그룹 관련 훅 추가했습니다.
    - 그룹 삭제 로직 생성
    - 그룹 업데이트 로직 생성
    - 그 외에 훅에 console.log 추가

--- src/hooks/useGroup.js
+++ src/hooks/useGroup.js
@@ -1,54 +1,118 @@
-import axios from 'axios';
-import { useSetRecoilState } from 'recoil';
-import { groupState, groupsState, seletGroupIdState } from '../recoil/groupAtoms';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import {
+  groupMarkersState,
+  groupRoutesState,
+  groupState,
+  groupsState,
+  seletGroupIdState,
+} from '../recoil/groupAtoms';
 import { useState } from 'react';
+import { instance } from '../api/customAxios';
 
 const useGroup = (groupId, groupTitle) => {
   const setGroups = useSetRecoilState(groupsState);
   const setGroup = useSetRecoilState(groupState);
-  const seletGroupId = useSetRecoilState(seletGroupIdState);
-  const [newGroupId, setNewGroupId] = useState(0);
+  const setGroupMarkers = useSetRecoilState(groupMarkersState);
+  const setGroupRoutes = useSetRecoilState(groupRoutesState);
+  const [seletGroupId, setSeletGroupId] = useRecoilState(seletGroupIdState);
   const [groupMembers, setGroupMembers] = useState([]);
-  const firstSeletGroupId = () => {};
+
   const getGroups = async () => {
     try {
-      const result = await axios.get('/group/getall');
+      const result = await instance.get('/group/getall');
       const copyGroups = [...result.data];
       setGroups(copyGroups);
+      console.log('그룹들 가져오기 : ', copyGroups);
+      if (seletGroupId === 0) {
+        setSeletGroupId(copyGroups[0].groupId);
+        console.log('최초 진입 시 그룹ID : ', seletGroupId);
+      } else {
+        setSeletGroupId(groupId);
+      }
+      console.log('지금 가지고 있는 그룹ID 스테이트 : ', seletGroupId);
     } catch (error) {
       console.log(error);
     }
   };
   const getGroup = async () => {
     try {
-      const result = await axios.get(`/group/get/${groupId}`);
-      setGroup(result.data);
+      const result = await instance.get(`/group/get/${groupId}`);
+      const copyGroup = result.data;
+      console.log('그룹 단건 조회 : ', copyGroup);
+      setGroup(copyGroup);
     } catch (error) {
       console.log(error);
     }
   };
   const getGroupmembers = async () => {
     try {
-      console.log(groupId);
-      const result = await axios.get(`/group/member/get/${groupId}`);
-      setGroupMembers(result.data);
-      console.log(result.data);
+      const result = await instance.get(`/group/member/get/${groupId}`);
+      const copyGroupMembers = [...result.data];
+      console.log('그룹 친구들 복사본 : ', copyGroupMembers);
+      setGroupMembers(copyGroupMembers);
     } catch (error) {
       console.log(error);
     }
   };
   const createGroup = async () => {
     try {
-      const result = await axios.post('/group/create', { groupTitle: groupTitle });
+      const result = await instance.post('/group/create', { groupTitle: groupTitle });
       console.log(result.data);
-      setNewGroupId(result.data.groupId);
+
       getGroups();
     } catch (error) {
       console.log(error);
     }
   };
+  const getGroupMarkers = async () => {
+    try {
+      const result = await instance.get(`/marker/group/${groupId}`);
+      const copyGroupMarkers = [...result.data];
+      console.log('그룹마커 복사본 : ', copyGroupMarkers);
+      setGroupMarkers(copyGroupMarkers);
+    } catch (error) {
+      console.log('그룹 마커 가져오기 실패', error);
+    }
+  };
+  const getGroupRoutes = async () => {
+    try {
+      const result = await instance.get(`/marker-route/group/${groupId}`);
+      const copyGroupRotes = [...result.data];
+      console.log('그룹마커 복사본 : ', copyGroupRotes);
+      setGroupRoutes(result.data);
+    } catch (error) {
+      console.log('그룹 루트 가져오기 실패', error);
+    }
+  };
+  const deletGroup = async () => {
+    try {
+      const result = await instance.delete(`/group/remove/${groupId}`);
+      console.log('그룹삭제 성공! : ', result);
+    } catch (error) {
+      console.log('그룹삭제 실패 : ', error);
+    }
+  };
+  const editGroup = async () => {
+    console.log('수정할 그룹명', groupTitle);
+    try {
+      const result = await instance.post(`/group/edit/${groupId}`, { title: groupTitle });
+      console.log('그룹수정 성공 : ', result);
+    } catch (error) {
+      console.log('그룹수정 실패', error);
+    }
+  };
 
-  return { getGroups, getGroup, getGroupmembers, groupMembers, createGroup };
+  return {
+    getGroups,
+    getGroup,
+    getGroupmembers,
+    groupMembers,
+    createGroup,
+    getGroupMarkers,
+    getGroupRoutes,
+    deletGroup,
+    editGroup,
+  };
 };
 
 export default useGroup;
